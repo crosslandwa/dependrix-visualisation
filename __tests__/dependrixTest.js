7fdd97cb9e19b655cc2c0965820eaf7a0175a206
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-14 23:21:36 +0100

    Build returns HTML with bundle.min.js inlined

--- __tests__/dependrixTest.js
+++ __tests__/dependrixTest.js
@@ -5,6 +5,8 @@ describe('Dependrix', () => {
     const html = Dependrix({}).then(html => {
       expect(html).toContain('<html lang="en">')
       expect(html).toContain('<title>Dependrix</title>')
+      expect(html).toMatch(/<script>\s*\(function \(\) {/)
+      expect(html).not.toContain('sourceMappingURL')
     }).then(done, done.fail)
   })
 })
--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const readFile = filename => new Promise((resolve, reject) => fs.readFile(
   (err, data) => err ? reject(err) : resolve(data.toString('utf-8'))
 ))
 
-export default () => readFile(`${__dirname}/dist/index.html`)
+export default () => Promise.all([
+  readFile(`${__dirname}/dist/index.html`),
+  readFile(`${__dirname}/dist/bundle.min.js`),
+]).then(([content, js]) => content.replace("<script src='bundle.min.js'></script>", `<script>\n${js.replace('//# sourceMappingURL=bundle.min.js.map', '')}\n<script>`))
