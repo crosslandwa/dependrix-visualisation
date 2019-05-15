4d7990318199251904df7ad380b28fb0261b63ef
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-15 18:48:08 +0100

    Display table of artifacts and dependencies

--- src/dependencies/DependencyMatrix.js
+++ src/dependencies/DependencyMatrix.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { artifactIds, artifactVersion } from './interactions'
+import { artifactDependencyScope, artifactDependencyVersion, artifactIds, artifactVersion, dependencyIds } from './interactions'
 
 const apply = (x, f) => f(x)
 
@@ -8,14 +8,44 @@ const mapStateToProps = state => apply(
   artifactIds(state),
   ids => ({
     ids,
-    versions: ids.reduce((acc, id) => ({ ...acc, [id]: artifactVersion(state, id) }), {})
+    versions: ids.reduce((acc, id) => ({ ...acc, [id]: artifactVersion(state, id) }), {}),
+    dependencyIds: dependencyIds(state)
   })
 )
 
-const DependencyMatrix = ({ ids, versions }) => (
-  <ul>{ids.map(id => (
-    <li>{id} - {versions[id]}</li>
-  ))}</ul>
+const mapStateToDependencyCellProps = (state, { artifactId, dependencyId }) => apply(
+  artifactDependencyVersion(state, artifactId, dependencyId),
+  version => ({
+    version,
+    scope: (version && artifactDependencyScope(state, artifactId, dependencyId)) || ''
+  })
+)
+
+const renderDependencyCell = ({ scope, version }) => version
+  ? <td>{version} ({scope})</td>
+  : <td></td>
+
+const DependencyCell = connect(mapStateToDependencyCellProps)(renderDependencyCell)
+
+const DependencyMatrix = ({ ids, versions, dependencyIds }) => (
+  <table>
+    <thead>
+      <tr>
+        <td></td>
+        {ids.map(id => (
+          <td>{id} - {versions[id]}</td>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {dependencyIds.map(dependencyId => (
+        <tr>
+          <td>{dependencyId}</td>
+          {ids.map(id => <DependencyCell artifactId={id} dependencyId={dependencyId}/>)}
+        </tr>
+      ))}
+    </tbody>
+  </table>
 )
 
 export default connect(mapStateToProps)(DependencyMatrix)
