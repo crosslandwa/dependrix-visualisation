9271030b99c7dfac322a8ec4993fe29354f131ed
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-22 22:52:52 +0100

    Validate model used in tests against schema

--- src/dependencies/__tests__/helpers.js
+++ src/dependencies/__tests__/helpers.js
@@ -1,3 +1,8 @@
+import Ajv from 'ajv'
+import fs from 'fs'
+
+const validateAgainstSchema = new Ajv().compile(JSON.parse(fs.readFileSync(`${__dirname}/../../../schema.json`, 'utf8')))
+
 export const clearModelFromDom = () => {
   document.getElementsByTagName('html')[0].innerHTML = ''
 }
@@ -8,7 +13,13 @@ export const injectModelIntoDom = model => {
 
 export const project = (id, version, ...dependencies) => ({ id, version, dependencies })
 export const dependency = (id, version, scope) => ({ id, version, scope })
-export const model = (...projects) => projects.reduce(
-  (acc, { id, dependencies, version }) => ({ projects: { ...acc.projects, [id]: { version, dependencies } } }),
-  { projects: {} }
-)
+export const model = (...projects) => {
+  const model = projects.reduce(
+    (acc, { id, dependencies, version }) => ({ projects: { ...acc.projects, [id]: { version, dependencies } } }),
+    { projects: {} }
+  )
+  if (!validateAgainstSchema(model)) {
+    throw new Error(`Supplied model failed validation: ${JSON.stringify(validateAgainstSchema.errors)}`)
+  }
+  return model
+}
--- src/dependencies/__tests__/treeLoadTest.js
+++ src/dependencies/__tests__/treeLoadTest.js
@@ -33,7 +33,7 @@ describe('Tree loading', () => {
   it('reports successful tree loading when model is available over HTTP', done => {
     const store = createStore()
 
-    mockGETRequest.mockImplementation(() => Promise.resolve('{"projects":{}}'))
+    mockGETRequest.mockImplementation(() => Promise.resolve(JSON.stringify(model())))
 
     store.dispatch(loadTree())
       .then(() => {
