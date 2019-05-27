8ba4952419b9bd16087b75ef5648577a9b2c3bf4
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-27 09:30:29 +0100

    Add division between multiple versions of same dependency

--- src/dependencies/DependencyCell.js
+++ src/dependencies/DependencyCell.js
@@ -3,11 +3,11 @@ import React from 'react'
 const DependencyCell = ({ dependencies }) => (
   <td class="matrix__table-cell">
     {dependencies.map(({ scope, version, versionLag }) => (
-      <React.Fragment>
+      <div class="matrix__dependency-detail">
         <span class={`matrix__table-cell-label${versionLag ? ` matrix__table-cell-label--${versionLag}` : ''}`}>{version}</span>
         {scope && <span class="matrix__table-cell-label">{scope}</span>}
-      </React.Fragment>
-    ))}
+      </div>
+    )).reduce((a, b) => a ? [a, <hr class="matrix__table-cell-divide"/>, b] : [b], null)}
   </td>
 )
 
